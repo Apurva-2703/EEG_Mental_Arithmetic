2d98f855dfa6adb2550966eab4fb50425775ca96
Apurva <apurvasingh27@gmail.com>
2025-04-17 03:42:19 +0000

    Add initial FFT setup with pyedflib

--- Cheat sheet.py
+++ Cheat sheet.py
@@ -2,14 +2,16 @@
 
 import os
 import pyedflib
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 
 #Select a subject (baseline or test file) and a channel
 edf_folder = "eeg-during-mental-arithmetic-tasks-1.0.0"
 baseline_path = os.path.join(edf_folder, "Subject00_1.edf") #Change subject number between 00 to 35
-selected_channel = "EEG Cz"
+f = pyedflib.EdfReader(baseline_path)
 
-#Follow script adapted from Ildar R. course on signal processing
-data_len =len()
+#Get signal labels
+labels = f.getSignalLabels()
+fz_index = labels.index("EEG Cz")
+
+f.getSampleFrequency(fz_index)
+
+
